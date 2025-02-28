b002038fcfb28e6fad5297956194abae92685821
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2025-02-28 07:20:22 +0000

    WIP: near-complete copy of GADTs from Haskell, very nice!!

--- util/src/rust_gadt_playground.rs
+++ util/src/rust_gadt_playground.rs
@@ -6,6 +6,7 @@
 //!
 //! data MyGADT a where
 //!   SomeFoo :: Foo a => a -> MyGADT a
+//!   SomeInt :: Int -> MyGADT Bool
 //!   Other :: a -> MyGADT a
 //!
 //! -- example usage
@@ -21,6 +22,31 @@
 //! ```
 //!
 //! For this I will use specialization and zero-sized type-witnesses
+//! Overall, the strategy seems to be to relate witnesses and trait-bounds
+//! And if you need to provide more than one trait bound, you can just add more and more witnesses
+//! Which will simply get compiled away
+//!
+//! We might even create a wrapper type like Witness<T, W>, where it will hold the witness type and
+//! the underlying type, which can allow simple trait bounds to be recovered:
+//!
+//! ```rust
+//! impl<T> Foo for Witness<T, FooWitness<T>> {
+//!   fn foo(&self) -> bool {
+//!     (self.0).foo_with(self.1) // this implementation gets replaced with original
+//!   }  
+//! }
+//! ```
+//!
+//! So this way, if you have e.g.
+//! ```rust
+//! fn something<T>(a: MyGADT<T>) -> impl Foo {
+//!   match a {
+//!     MyGADT::SomeFoo(wit, a) => Witness { wit, a }, // Witness { wit, a } implements `Foo`
+//!     _ => unimplemented!(),
+//!   }
+//! }
+//! ```
+//!
 
 use std::marker::PhantomData;
 
@@ -28,9 +54,11 @@ pub trait Foo {
     fn foo(&self) -> bool;
 }
 
+/// This is the GADT being simulated
 #[derive(Copy, Clone, Eq, PartialEq, Debug, Hash)]
 pub enum MyGADT<T> {
     SomeFoo(FooWitness<T>, T),
+    SomeInt(TyEqWitness<T, bool>, usize),
     Other(T),
 }
 
@@ -85,10 +113,63 @@ pub mod ty_bool {
 // Type-level equality, useful for later
 pub trait TyEq {
     type Rhs: TyEq<Rhs = Self> + ?Sized;
+
+    fn into_ty_eq(self) -> Self::Rhs
+    where
+        Self: Sized;
+
+    fn from_ty_eq(rhs: Self::Rhs) -> Self
+    where
+        Self::Rhs: Sized;
+
+    fn ty_eq_ref(&self) -> &Self::Rhs;
+
+    fn from_ty_eq_ref(rhs: &Self::Rhs) -> &Self;
+
+    fn ty_eq_mut(&mut self) -> &mut Self::Rhs;
+
+    fn from_ty_eq_mut(rhs: &mut Self::Rhs) -> &mut Self;
 }
 
+#[allow(clippy::inline_always)]
 impl<T: ?Sized> TyEq for T {
     type Rhs = T;
+
+    #[inline(always)]
+    fn into_ty_eq(self) -> T
+    where
+        T: Sized,
+    {
+        self
+    }
+
+    #[inline(always)]
+    fn from_ty_eq(rhs: T) -> T
+    where
+        T: Sized,
+    {
+        rhs
+    }
+
+    #[inline(always)]
+    fn ty_eq_ref(&self) -> &T {
+        self
+    }
+
+    #[inline(always)]
+    fn from_ty_eq_ref(rhs: &T) -> &T {
+        rhs
+    }
+
+    #[inline(always)]
+    fn ty_eq_mut(&mut self) -> &mut T {
+        self
+    }
+
+    #[inline(always)]
+    fn from_ty_eq_mut(rhs: &mut T) -> &mut T {
+        rhs
+    }
 }
 
 /// This is an instance provider trait for [`Foo`], it uses specialization to provide the correct
@@ -124,22 +205,183 @@ impl Foo for usize {
     }
 }
 
+/// This is a type-witness for [`TyEq`], it can only be constructed for types that implement [`TyEq`]
+/// for a specific [`Rhs`] type.
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Hash)]
+#[repr(transparent)]
+pub struct TyEqWitness<L: ?Sized, R: ?Sized>(PhantomData<L>, PhantomData<R>);
+
+// In general, we can reuse witness structs as follows:
+//
+// Witness1<T>
+// multiple impl blocks containing smart constructors for these witnesses ??
+
+impl<L: ?Sized, R: ?Sized> TyEqWitness<L, R> {
+    #[inline]
+    #[must_use]
+    pub const fn new() -> Self
+    where
+        L: TyEq<Rhs = R>,
+    {
+        Self(PhantomData, PhantomData)
+    }
+}
+
+impl<L: TyEq<Rhs = R> + ?Sized, R: ?Sized> Default for TyEqWitness<L, R> {
+    #[inline]
+    #[must_use]
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[inline]
+#[must_use]
+pub const fn is_ty_eq<L: TyEq<Rhs = R> + ?Sized, R: ?Sized>() -> TyEqWitness<L, R> {
+    TyEqWitness::new()
+}
+
+/// This is an instance provider trait for [`TyEq`], it uses specialization to provide the correct
+/// implementation of [`TyEq`] with static-dispatch, based on a supplied [`TyEqWitness`] witness value.
+pub trait TyEqInstance<Rhs: ?Sized> {
+    type Implemented: ty_bool::Bool;
+
+    fn into_ty_eq_with(self, ty_eq_witness: &TyEqWitness<Self, Rhs>) -> Rhs
+    where
+        Self: Sized,
+        Rhs: Sized;
+
+    fn from_ty_eq_with(rhs: Rhs, ty_eq_witness: &TyEqWitness<Self, Rhs>) -> Self
+    where
+        Self: Sized,
+        Rhs: Sized;
+
+    fn ty_eq_ref_with(&self, ty_eq_witness: &TyEqWitness<Self, Rhs>) -> &Rhs;
+
+    fn from_ty_eq_ref_with<'a>(rhs: &'a Rhs, ty_eq_witness: &TyEqWitness<Self, Rhs>) -> &'a Self;
+
+    fn ty_eq_mut_with(&mut self, ty_eq_witness: &TyEqWitness<Self, Rhs>) -> &mut Rhs;
+
+    fn from_ty_eq_mut_with<'a>(
+        rhs: &'a mut Rhs,
+        ty_eq_witness: &TyEqWitness<Self, Rhs>,
+    ) -> &'a mut Self;
+}
+
+impl<L: ?Sized, R: ?Sized> TyEqInstance<R> for L {
+    default type Implemented = ty_bool::False;
+
+    #[inline]
+    default fn into_ty_eq_with(self, _ty_eq_witness: &TyEqWitness<L, R>) -> R
+    where
+        L: Sized,
+        R: Sized,
+    {
+        unreachable!()
+    }
+
+    #[inline]
+    default fn from_ty_eq_with(_rhs: R, _ty_eq_witness: &TyEqWitness<L, R>) -> Self
+    where
+        L: Sized,
+        R: Sized,
+    {
+        unreachable!()
+    }
+
+    #[inline]
+    default fn ty_eq_ref_with(&self, _ty_eq_witness: &TyEqWitness<L, R>) -> &R {
+        unreachable!()
+    }
+
+    #[inline]
+    default fn from_ty_eq_ref_with<'a>(
+        _rhs: &'a R,
+        _ty_eq_witness: &TyEqWitness<L, R>,
+    ) -> &'a Self {
+        unreachable!()
+    }
+
+    #[inline]
+    default fn ty_eq_mut_with(&mut self, _ty_eq_witness: &TyEqWitness<L, R>) -> &mut R {
+        unreachable!()
+    }
+
+    #[inline]
+    default fn from_ty_eq_mut_with<'a>(
+        _rhs: &'a mut R,
+        _ty_eq_witness: &TyEqWitness<L, R>,
+    ) -> &'a mut Self {
+        unreachable!()
+    }
+}
+
+impl<L: TyEq<Rhs = R> + ?Sized, R: ?Sized> TyEqInstance<R> for L {
+    default type Implemented = ty_bool::False;
+
+    #[inline]
+    default fn into_ty_eq_with(self, _ty_eq_witness: &TyEqWitness<L, R>) -> R
+    where
+        L: Sized,
+        R: Sized,
+    {
+        self.into_ty_eq()
+    }
+
+    #[inline]
+    default fn from_ty_eq_with(rhs: R, _ty_eq_witness: &TyEqWitness<L, R>) -> Self
+    where
+        L: Sized,
+        R: Sized,
+    {
+        Self::from_ty_eq(rhs)
+    }
+
+    #[inline]
+    default fn ty_eq_ref_with(&self, _ty_eq_witness: &TyEqWitness<L, R>) -> &R {
+        self.ty_eq_ref()
+    }
+
+    #[inline]
+    default fn from_ty_eq_ref_with<'a>(rhs: &'a R, _ty_eq_witness: &TyEqWitness<L, R>) -> &'a Self {
+        Self::from_ty_eq_ref(rhs)
+    }
+
+    #[inline]
+    default fn ty_eq_mut_with(&mut self, _ty_eq_witness: &TyEqWitness<L, R>) -> &mut R {
+        self.ty_eq_mut()
+    }
+
+    #[inline]
+    default fn from_ty_eq_mut_with<'a>(
+        rhs: &'a mut R,
+        _ty_eq_witness: &TyEqWitness<L, R>,
+    ) -> &'a mut Self {
+        Self::from_ty_eq_mut(rhs)
+    }
+}
+
 // function which makes use of `MyGADT` in pattern matching:
 // by construction, a value of type `FooWitness<T>` can only be inhabited if `T: Foo`
 // so `FooInstance::foo_with` will __always__ specialize to the one that doesn't panic
 #[inline]
-pub fn do_thing<T>(a: &MyGADT<T>) -> bool {
+pub fn do_thing<T>(a: &MyGADT<T>, b: T) -> bool {
     match *a {
         MyGADT::SomeFoo(ref wit, ref a) => a.foo_with(wit),
+        MyGADT::SomeInt(ref wit, ref a) => {
+            // at this point, I _know_ what type `T` is `bool`, hence:
+            let b = <T as TyEqInstance<bool>>::into_ty_eq_with(b, wit);
+
+            // I can now directly return `b` for example
+            b
+        }
         MyGADT::Other(_) => false,
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::rust_gadt_playground::{
-        do_thing, is_foo, ty_bool, FooInstance, MyGADT,
-    };
+    use crate::rust_gadt_playground::{do_thing, is_foo, ty_bool, FooInstance, MyGADT};
 
     #[test]
     fn test_foo() {
@@ -154,6 +396,8 @@ mod tests {
         let gadt_value = MyGADT::SomeFoo(usize_witness, arg);
 
         // this never panics
-        assert!(do_thing(&gadt_value)); // OK
+        assert!(do_thing(&gadt_value, 23)); // OK
     }
 }
+
+// We can use type-equality to implement type equality constraints
