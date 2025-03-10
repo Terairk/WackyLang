ea3e23ba71ba6f3f3024f362f97604b670f2c429
Ahmad Jamsari <akj23@ic.ac.uk>
2025-03-10 15:05:57 +0000

    implement(optimization) - folding constant for unary operators

--- middle/src/optimizations/constant_folding.rs
+++ middle/src/optimizations/constant_folding.rs
@@ -1,13 +1,13 @@
 use crate::wackir::{
     UnaryOp::{self, BNot, Chr, LNot, Len, Negate, Ord},
-    WackChar,
+    WackBool, WackChar,
     WackInstr::{self, Binary, Copy, JumpIfNotZero, JumpIfZero, JumpToHandler, Unary},
     WackLiteral::{self, Char},
     WackTempIdent,
     WackValue::{self, Literal},
 };
 
-use util::gen_flags::{GenFlags, INBUILT_BAD_CHAR, insert_flag_gbl};
+use util::gen_flags::{GenFlags, INBUILT_BAD_CHAR, INBUILT_OVERFLOW, insert_flag_gbl};
 
 // Constants for the bounds of the chr function
 const MAX_CHR: i32 = 127;
@@ -34,13 +34,11 @@ pub(crate) fn constant_fold_function(function_body: Vec<WackInstr>) -> Vec<WackI
         let new_instrs = match instr {
             Unary { op, src, dst } => eval_unary_op(op, src, dst),
             Binary {
-                ref op,
-                ref src1,
-                ref src2,
-                ref dst,
-            } => {
-                unimplemented!()
-            }
+                op,
+                src1,
+                src2,
+                dst,
+            } => unimplemented!(),
             JumpIfZero {
                 ref condition,
                 ref target,
@@ -65,27 +63,63 @@ pub(crate) fn constant_fold_function(function_body: Vec<WackInstr>) -> Vec<WackI
 fn eval_unary_op(op: UnaryOp, src: WackValue, dst: WackTempIdent) -> Vec<WackInstr> {
     if let WackValue::Literal(value) = src {
         match op {
-            Chr => eval_chr(value, dst),
-            _ => unimplemented!(),
+            Chr => eval_chr(&value, dst),
+            Ord => {
+                if let WackLiteral::Char(value) = value {
+                    vec![Copy {
+                        src: Literal(WackLiteral::Int(i32::from(value.into_u8()))),
+                        dst,
+                    }]
+                } else {
+                    panic!("Invalid value passed to ord. Indicates an error in the front-end");
+                }
+            }
+            Len => {
+                if let WackLiteral::StringLit(value) = value {
+                    // Safe to unwrap because wacc strings should fit into an i32
+                    vec![Copy {
+                        src: Literal(WackLiteral::Int(
+                            i32::try_from(value.len()).expect("String length is too large"),
+                        )),
+                        dst,
+                    }]
+                } else {
+                    panic!("Invalid value passed to len. Indicates an error in the front-end");
+                }
+            }
+            Negate => eval_negate(&value, dst),
+            BNot => todo!("Implement bitwise not"),
+            LNot => {
+                let new_instr = match value {
+                    WackLiteral::Bool(value) => {
+                        let negated: WackBool = (!value.into_bool()).into();
+                        Copy {
+                            src: Literal(WackLiteral::Bool(negated)),
+                            dst,
+                        }
+                    }
+                    _ => {
+                        panic!("Invalid value passed to lnot. Indicates an error in the front-end")
+                    }
+                };
+
+                vec![new_instr]
+            }
         }
     } else {
-        vec![Unary {
-            op,
-            src: src.clone(),
-            dst: dst.clone(),
-        }]
+        vec![Unary { op, src, dst }]
     }
 }
 
-fn eval_chr(value: WackLiteral, dst: WackTempIdent) -> Vec<WackInstr> {
-    if let WackLiteral::Int(value) = value {
+fn eval_chr(value: &WackLiteral, dst: WackTempIdent) -> Vec<WackInstr> {
+    if let WackLiteral::Int(ref value) = *value {
         // chr has some runtime checks which we can do at compile time
-        let new_instr = if value >= MIN_CHR && value <= MAX_CHR {
+        let new_instr = if (MIN_CHR..=MAX_CHR).contains(value) {
             // Safe to unwrap because we know the value is within bounds
-            let char = unsafe { WackChar::from_u8_unchecked(value as u8) };
+            let char = unsafe { WackChar::from_u8_unchecked(*value as u8) };
             Copy {
                 src: Literal(Char(char)),
-                dst: dst.clone(),
+                dst,
             }
         } else {
             insert_flag_gbl(GenFlags::CHR_BOUNDS);
@@ -95,5 +129,24 @@ fn eval_chr(value: WackLiteral, dst: WackTempIdent) -> Vec<WackInstr> {
         vec![new_instr]
     } else {
         panic!("Invalid value passed to chr. Indicates an error in the front-end");
+    }
+}
+
+fn eval_negate(value: &WackLiteral, dst: WackTempIdent) -> Vec<WackInstr> {
+    if let WackLiteral::Int(value) = value {
+        let new_instr = match value.checked_neg() {
+            Some(value) => Copy {
+                src: Literal(WackLiteral::Int(value)),
+                dst,
+            },
+            None => {
+                insert_flag_gbl(GenFlags::OVERFLOW);
+                JumpToHandler(INBUILT_OVERFLOW.to_owned())
+            }
+        };
+
+        vec![new_instr]
+    } else {
+        panic!("Invalid value passed to negate. Indicates an error in the front-end");
     }
 }
